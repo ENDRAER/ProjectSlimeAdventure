acedae375e5dd188971663dae53ec7393c6151ea
ENDRAER <77845142+ENDRAER@users.noreply.github.com>
2023-08-24 20:41:39 +0300

    0.0.3

--- Assets/Other/FieldGrid.cs
+++ Assets/Other/FieldGrid.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class FieldGrid : MonoBehaviour
 {
     [SerializeField] private GameObject[] AllCellsOnTheField;
-    [NonSerialized] public CellParameters[,] MovingGrid;
+    [NonSerialized] public CellParameters[,] MovingGrid = new CellParameters[100,100];
 
     private void Awake()
     {
         foreach (GameObject go in AllCellsOnTheField)
         {
-            print(go.name);
-            print(go.GetComponent<CellParameters>());
-            MovingGrid[(int)go.transform.position.x, (int)go.transform.position.z] = go.GetComponent<CellParameters>();
+            MovingGrid[(int)go.transform.position.x + 50, (int)go.transform.position.z + 50] = go.GetComponent<CellParameters>();
         }
     }
 }
--- Assets/Slime/SlimeMoving.cs
+++ Assets/Slime/SlimeMoving.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class SlimeMoving : MonoBehaviour
 {
     [SerializeField] private Animator m_Animator;
+    [SerializeField] private FieldGrid fieldGrid;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && fieldGrid.MovingGrid[(int)transform.position.x + 50, (int)transform.position.z + 51] != null)
             m_Animator.SetTrigger("MoveUp");
-        else if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A) && fieldGrid.MovingGrid[(int)transform.position.x + 49, (int)transform.position.z + 50] != null)
             m_Animator.SetTrigger("MoveLeft");
-        else if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S) && fieldGrid.MovingGrid[(int)transform.position.x + 50, (int)transform.position.z + 49] != null)
             m_Animator.SetTrigger("MoveDown");
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.D) && fieldGrid.MovingGrid[(int)transform.position.x + 51, (int)transform.position.z + 50] != null)
             m_Animator.SetTrigger("MoveRight");
     }
 
