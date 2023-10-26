9fe2aa7c51c2299cecf3c5c9e2ffc3de2f282383
ENDRAER <77845142+ENDRAER@users.noreply.github.com>
2023-10-26 17:03:19 +0300

    0.6.0

--- Assets/Levels/0_Default/Scripts/Cells/CellFinish.cs
+++ Assets/Levels/0_Default/Scripts/Cells/CellFinish.cs
@@ -1,10 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class CellFinish : CellParameters
 {
     public override void Restart()
+    {
+
+    }
+
+    public override void LandingBehaviour(GameObject SlimeGO)
     {
 
     }
